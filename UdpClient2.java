a6106f303c62616d65da2a810a7261cc76e93042
Akrasniqii <93943782+Akrasniqii@users.noreply.github.com>
2022-04-11 23:22:23 +0200

    Add files via upload

--- UdpClient2.java
+++ UdpClient2.java
@@ -7,5 +7,10 @@ public class UdpClient2 {
     public static void main(String[] args) throws SocketException, UnknownHostException, IOException {
 
         Scanner input = new Scanner(System.in);
+        // Krijimi i socket
+        DatagramSocket diagramiSocket = new DatagramSocket();
 
-       
+        // Destimi i IP Serverit
+        InetAddress ipAdresa = InetAddress.getByName("localhost");
+
+      
