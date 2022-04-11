209ab2751373021f8f187790e5e068ca0683aa1b
Akrasniqii <93943782+Akrasniqii@users.noreply.github.com>
2022-04-11 23:20:36 +0200

    Add files via upload

---
+++ UdpClient2.java
@@ -0,0 +1,11 @@
+import java.net.*;
+import java.io.*;
+import java.util.Scanner;
+import java.nio.charset.StandardCharsets;
+
+public class UdpClient2 {
+    public static void main(String[] args) throws SocketException, UnknownHostException, IOException {
+
+        Scanner input = new Scanner(System.in);
+
+       
